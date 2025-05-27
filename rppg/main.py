9d110b81b0fa84b5520285d2b3d70a9841b66365
Rahmat Aldi Nasda <145269283+urbaee@users.noreply.github.com>
2025-05-27 21:01:18 +0700

    minor

--- rppg/main.py
+++ rppg/main.py
@@ -4,12 +4,10 @@ from rppg.camera_selector import CameraSelector
 from ui.main_window import MainWindow
 
 if __name__ == "__main__":
-#    app = QtWidgets.QApplication(sys.argv)
-#    selector = CameraSelector()
-#    if selector.exec() == QtWidgets.QDialog.DialogCode.Accepted:
-#        selected_camera = selector.get_selected_camera()
-#        window = MainWindow(camera_index=selected_camera)
-#        window.show()
-#        sys.exit(app.exec())
-    video_thread = VideoThread()
-    video_thread.start()
+    app = QtWidgets.QApplication(sys.argv)
+    selector = CameraSelector()
+    if selector.exec() == QtWidgets.QDialog.DialogCode.Accepted:
+        selected_camera = selector.get_selected_camera()
+        window = MainWindow(camera_index=selected_camera)
+        window.show()
+        sys.exit(app.exec())
